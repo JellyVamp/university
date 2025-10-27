d49c59f3297ee97a2622caaced18e93206711c87
Герасимов Александр Евгеньевич ИП-513 <adminlinux@castle.localdomain>
2025-10-27 21:41:22 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@ int main(int argc, char** argv) {
     char* str = (char*)malloc(sizeof(char) * SIZE);
 
     printf("Введите своё имя:\n");
-    str = gets(str);
-    printf("Привет, %s", str);
+    scanf("%s", str);
+    puts("Привет");
+    puts(str);
     
     return 0;
 }
